3bb7cd962cb95997c151a8ccf8ae250ea96580c1
Gerard <Gerard@TORRE>
2023-10-07 15:55:06 +0200

    nivell 1 exercici 2

---
+++ Herencia/src/n1exercici2/Cotxe.java
@@ -0,0 +1,19 @@
+package n1exercici2;
+
+public class Cotxe {
+	
+	private static final String marca = "toyota";
+	private static String model = "xaxo";
+	private final int potencia = 999;
+	
+	public static String frenar(Cotxe cotxe) {
+		
+		return "estic frenant";
+	}
+	
+	public String accelerar(){
+		
+		return "estic accelerant";
+	}
+
+}
---
+++ Herencia/src/n1exercici2/MainClass.java
@@ -0,0 +1,14 @@
+package n1exercici2;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		Cotxe vehicle1 = new Cotxe();
+		
+		System.out.println(Cotxe.frenar(vehicle1));
+		System.out.println(vehicle1.accelerar());
+		
+	}
+
+}
